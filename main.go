de24a32f3ff209291bfa1b49ca5eec242aefb729
Charles Perniciaro III <blackbeltcharles@gmail.com>
2021-10-12 22:35:13 -0500

    added sprites json and collision detection

--- main.go
+++ main.go
@@ -1,254 +1,163 @@
 package main
 
 import (
+	"encoding/json"
 	"image"
 	_ "image/png"
 	"log"
+	"os"
+	"strings"
 
 	"github.com/hajimehoshi/ebiten"
 	"github.com/hajimehoshi/ebiten/ebitenutil"
 )
 
-var x int
-var y int
-var keyBuf ebiten.Key
-var lastDir ebiten.Key
-var imgFrameNum int
-var animation bool
-var img Sprite
-var linkSprites map[string]Sprite
-var op *ebiten.DrawImageOptions
+// Game is an ebiten Game interface implemetation plus custom struct data
+type Game struct {
+	Player  Player
+	Sprites map[string]Sprite
+	Options *ebiten.DrawImageOptions
+}
 
+// Player represents the player character
+type Player struct {
+	X         int        // The current X screen offset of the player
+	Y         int        // The current Y screen offset of the player
+	Animation bool       // Whether or not the player is in a special animation or the normal stand/walk cycle.
+	KeyBuf    ebiten.Key // The key pressed on the last frame or -1
+	LastDir   ebiten.Key // The last direction the player faced (never -1)
+	Sprite    Sprite     // The current sprite for the player
+	FrameNum  int        // The current frame of the sprite for the player
+}
+
+// Sprite represents an image with a number of sub-frames in it to be rendered via rectangles
 type Sprite struct {
-	FrameLen    int
-	FrameHeight int
 	FrameWidth  int
+	FrameHeight int
+	FrameLen    int // How many frames are in the sprite
 	Image       *ebiten.Image
 }
 
-func init() {
-	x = 0
-	y = 0
-	imgFrameNum = 0
-	keyBuf = -1
-	lastDir = ebiten.KeyDown
-	animation = false
-	op = &ebiten.DrawImageOptions{}
-
-	linkSprites = map[string]Sprite{}
-
-	linkStandSouth, _, err := ebitenutil.NewImageFromFile("link_stand_south.png", 0)
-	if err != nil {
-		log.Fatal(err)
-	}
-	linkSprites["linkStandSouth"] = Sprite{
-		FrameLen:    1,
-		FrameHeight: 26,
-		FrameWidth:  16,
-		Image:       linkStandSouth,
-	}
-
-	linkStandNorth, _, err := ebitenutil.NewImageFromFile("link_stand_north.png", 0)
-	if err != nil {
-		log.Fatal(err)
-	}
-	linkSprites["linkStandNorth"] = Sprite{
-		FrameLen:    1,
-		FrameHeight: 26,
-		FrameWidth:  16,
-		Image:       linkStandNorth,
-	}
-
-	linkStandWest, _, err := ebitenutil.NewImageFromFile("link_stand_west.png", 0)
-	if err != nil {
-		log.Fatal(err)
-	}
-	linkSprites["linkStandWest"] = Sprite{
-		FrameLen:    1,
-		FrameHeight: 24,
-		FrameWidth:  17,
-		Image:       linkStandWest,
-	}
-
-	linkStandEast, _, err := ebitenutil.NewImageFromFile("link_stand_east.png", 0)
-	if err != nil {
-		log.Fatal(err)
-	}
-	linkSprites["linkStandEast"] = Sprite{
-		FrameLen:    1,
-		FrameHeight: 24,
-		FrameWidth:  17,
-		Image:       linkStandEast,
-	}
-
-	linkWalkSouth, _, err := ebitenutil.NewImageFromFile("link_walk_south.png", 0)
-	if err != nil {
-		log.Fatal(err)
-	}
-	linkSprites["linkWalkSouth"] = Sprite{
-		FrameLen:    8,
-		FrameHeight: 26,
-		FrameWidth:  16,
-		Image:       linkWalkSouth,
-	}
-
-	linkWalkNorth, _, err := ebitenutil.NewImageFromFile("link_walk_north.png", 0)
-	if err != nil {
-		log.Fatal(err)
-	}
-	linkSprites["linkWalkNorth"] = Sprite{
-		FrameLen:    8,
-		FrameHeight: 26,
-		FrameWidth:  16,
-		Image:       linkWalkNorth,
-	}
-
-	linkWalkWest, _, err := ebitenutil.NewImageFromFile("link_walk_west.png", 0)
-	if err != nil {
-		log.Fatal(err)
-	}
-	linkSprites["linkWalkWest"] = Sprite{
-		FrameLen:    8,
-		FrameHeight: 25,
-		FrameWidth:  18,
-		Image:       linkWalkWest,
-	}
-
-	linkWalkEast, _, err := ebitenutil.NewImageFromFile("link_walk_east.png", 0)
-	if err != nil {
-		log.Fatal(err)
-	}
-	linkSprites["linkWalkEast"] = Sprite{
-		FrameLen:    8,
-		FrameHeight: 25,
-		FrameWidth:  18,
-		Image:       linkWalkEast,
-	}
-
-	linkAttackSouth, _, err := ebitenutil.NewImageFromFile("link_attack_south.png", 0)
-	if err != nil {
-		log.Fatal(err)
-	}
-	linkSprites["linkAttackSouth"] = Sprite{
-		FrameLen:    8,
-		FrameHeight: 24,
-		FrameWidth:  16,
-		Image:       linkAttackSouth,
-	}
-
-	linkAttackNorth, _, err := ebitenutil.NewImageFromFile("link_attack_north.png", 0)
-	if err != nil {
-		log.Fatal(err)
-	}
-	linkSprites["linkAttackNorth"] = Sprite{
-		FrameLen:    8,
-		FrameHeight: 29,
-		FrameWidth:  16,
-		Image:       linkAttackNorth,
-	}
-
-	linkAttackWest, _, err := ebitenutil.NewImageFromFile("link_attack_west.png", 0)
-	if err != nil {
-		log.Fatal(err)
-	}
-	linkSprites["linkAttackWest"] = Sprite{
-		FrameLen:    8,
-		FrameHeight: 24,
-		FrameWidth:  20,
-		Image:       linkAttackWest,
-	}
-
-	linkAttackEast, _, err := ebitenutil.NewImageFromFile("link_attack_east.png", 0)
-	if err != nil {
-		log.Fatal(err)
-	}
-	linkSprites["linkAttackEast"] = Sprite{
-		FrameLen:    8,
-		FrameHeight: 24,
-		FrameWidth:  20,
-		Image:       linkAttackEast,
-	}
-
-	img = linkSprites["linkStandSouth"]
+// SpriteJSON represents the json to be read from the sprite json file.
+type SpriteJSON struct {
+	FrameWidth  int    `json:"frameWidth"`
+	FrameHeight int    `json:"frameHeight"`
+	FrameLen    int    `json:"frameLen"`
+	Image       string `json:"image"`
 }
 
-type Game struct{}
+type Rect struct {
+	X int
+	Y int
+	W int
+	H int
+}
+
+// IsCollision checks if each vertex of rectangle is inside another rectangle
+func IsCollision(r0, r1 Rect) bool {
+	return r0.X > r1.X && r0.X < r1.X+r1.W && r0.Y > r1.Y && r0.Y < r1.Y+r1.H ||
+		r0.X+r0.W > r1.X && r0.X+r0.W < r1.X+r1.W && r0.Y > r1.Y && r0.Y < r1.Y+r1.H ||
+		r0.X > r1.X && r0.X < r1.X+r1.W && r0.Y+r0.H > r1.Y && r0.Y+r0.H < r1.Y+r1.H ||
+		r0.X+r0.W > r1.X && r0.X+r0.W < r1.X+r1.W && r0.Y+r0.H > r1.Y && r0.Y+r0.H < r1.Y+r1.H
+}
 
 func (g *Game) Update(screen *ebiten.Image) error {
+	playerRect := Rect{
+		X: g.Player.X,
+		Y: g.Player.Y,
+		W: g.Player.Sprite.FrameWidth,
+		H: g.Player.Sprite.FrameHeight,
+	}
 
-	if img.FrameLen > 1 {
-		imgFrameNum++
-		imgFrameNum = imgFrameNum % (img.FrameLen - 1)
+	otherRect := Rect{
+		X: 100,
+		Y: 100,
+		W: 32,
+		H: 32,
+	}
+
+	if g.Player.Sprite.FrameLen > 1 {
+		g.Player.FrameNum++
+		// FrameNum is zero indexed and FrameLen is a natural number, so subtract 1 for the mod operation
+		g.Player.FrameNum = g.Player.FrameNum % (g.Player.Sprite.FrameLen - 1)
 		// End the animation if the last render was the last frame
-		if animation && imgFrameNum == 0 {
-			animation = false
+		if g.Player.Animation && g.Player.FrameNum == 0 {
+			g.Player.Animation = false
 		}
 	}
 
 	if ebiten.IsKeyPressed(ebiten.KeyLeft) {
-		if keyBuf != ebiten.KeyLeft {
-			keyBuf = ebiten.KeyLeft
-			lastDir = ebiten.KeyLeft
-			imgFrameNum = 0
-			img = linkSprites["linkWalkWest"]
+		if g.Player.KeyBuf != ebiten.KeyLeft {
+			g.Player.KeyBuf = ebiten.KeyLeft
+			g.Player.LastDir = ebiten.KeyLeft
+			g.Player.FrameNum = 0
+			g.Player.Sprite = g.Sprites["linkWalkWest"]
 		}
-		x--
+		if !IsCollision(playerRect, otherRect) {
+			g.Player.X--
+		}
 	} else if ebiten.IsKeyPressed(ebiten.KeyRight) {
-		if keyBuf != ebiten.KeyRight {
-			keyBuf = ebiten.KeyRight
-			lastDir = ebiten.KeyRight
-			imgFrameNum = 0
-			img = linkSprites["linkWalkEast"]
+		if g.Player.KeyBuf != ebiten.KeyRight {
+			g.Player.KeyBuf = ebiten.KeyRight
+			g.Player.LastDir = ebiten.KeyRight
+			g.Player.FrameNum = 0
+			g.Player.Sprite = g.Sprites["linkWalkEast"]
 		}
-		x++
+		if !IsCollision(playerRect, otherRect) {
+			g.Player.X++
+		}
 	} else if ebiten.IsKeyPressed(ebiten.KeyUp) {
-		if keyBuf != ebiten.KeyUp {
-			keyBuf = ebiten.KeyUp
-			lastDir = ebiten.KeyUp
-			imgFrameNum = 0
-			img = linkSprites["linkWalkNorth"]
+		if g.Player.KeyBuf != ebiten.KeyUp {
+			g.Player.KeyBuf = ebiten.KeyUp
+			g.Player.LastDir = ebiten.KeyUp
+			g.Player.FrameNum = 0
+			g.Player.Sprite = g.Sprites["linkWalkNorth"]
 		}
-		y--
+		if !IsCollision(playerRect, otherRect) {
+			g.Player.Y--
+		}
 	} else if ebiten.IsKeyPressed(ebiten.KeyDown) {
-		if keyBuf != ebiten.KeyDown {
-			keyBuf = ebiten.KeyDown
-			lastDir = ebiten.KeyDown
-			imgFrameNum = 0
-			img = linkSprites["linkWalkSouth"]
+		if g.Player.KeyBuf != ebiten.KeyDown {
+			g.Player.KeyBuf = ebiten.KeyDown
+			g.Player.LastDir = ebiten.KeyDown
+			g.Player.FrameNum = 0
+			g.Player.Sprite = g.Sprites["linkWalkSouth"]
 		}
-		y++
+		if !IsCollision(playerRect, otherRect) {
+			g.Player.Y++
+		}
 	} else {
-		if !animation && lastDir == ebiten.KeyLeft {
-			keyBuf = -1
-			imgFrameNum = 0
-			img = linkSprites["linkStandWest"]
-		} else if !animation && lastDir == ebiten.KeyRight {
-			keyBuf = -1
-			imgFrameNum = 0
-			img = linkSprites["linkStandEast"]
-		} else if !animation && lastDir == ebiten.KeyUp {
-			keyBuf = -1
-			imgFrameNum = 0
-			img = linkSprites["linkStandNorth"]
-		} else if !animation && lastDir == ebiten.KeyDown {
-			keyBuf = -1
-			imgFrameNum = 0
-			img = linkSprites["linkStandSouth"]
+		if !g.Player.Animation && g.Player.LastDir == ebiten.KeyLeft {
+			g.Player.KeyBuf = -1
+			g.Player.FrameNum = 0
+			g.Player.Sprite = g.Sprites["linkStandWest"]
+		} else if !g.Player.Animation && g.Player.LastDir == ebiten.KeyRight {
+			g.Player.KeyBuf = -1
+			g.Player.FrameNum = 0
+			g.Player.Sprite = g.Sprites["linkStandEast"]
+		} else if !g.Player.Animation && g.Player.LastDir == ebiten.KeyUp {
+			g.Player.KeyBuf = -1
+			g.Player.FrameNum = 0
+			g.Player.Sprite = g.Sprites["linkStandNorth"]
+		} else if !g.Player.Animation && g.Player.LastDir == ebiten.KeyDown {
+			g.Player.KeyBuf = -1
+			g.Player.FrameNum = 0
+			g.Player.Sprite = g.Sprites["linkStandSouth"]
 		}
 	}
 
 	// If starting an animation
-	if !animation && ebiten.IsKeyPressed(ebiten.KeySpace) {
-		animation = true
-		imgFrameNum = 0
-		if lastDir == ebiten.KeyLeft {
-			img = linkSprites["linkAttackWest"]
-		} else if lastDir == ebiten.KeyRight {
-			img = linkSprites["linkAttackEast"]
-		} else if lastDir == ebiten.KeyUp {
-			img = linkSprites["linkAttackNorth"]
-		} else if lastDir == ebiten.KeyDown {
-			img = linkSprites["linkAttackSouth"]
+	if !g.Player.Animation && ebiten.IsKeyPressed(ebiten.KeySpace) {
+		g.Player.Animation = true
+		g.Player.FrameNum = 0
+		if g.Player.LastDir == ebiten.KeyLeft {
+			g.Player.Sprite = g.Sprites["linkAttackWest"]
+		} else if g.Player.LastDir == ebiten.KeyRight {
+			g.Player.Sprite = g.Sprites["linkAttackEast"]
+		} else if g.Player.LastDir == ebiten.KeyUp {
+			g.Player.Sprite = g.Sprites["linkAttackNorth"]
+		} else if g.Player.LastDir == ebiten.KeyDown {
+			g.Player.Sprite = g.Sprites["linkAttackSouth"]
 		}
 	}
 
@@ -256,10 +165,14 @@ func (g *Game) Update(screen *ebiten.Image) error {
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	op.GeoM.Reset()
-	op.GeoM.Translate(float64(x), float64(y))
+	g.Options.GeoM.Reset()
+	g.Options.GeoM.Translate(100, 100)
+	screen.DrawImage(g.Sprites["stump"].Image, g.Options)
+
+	g.Options.GeoM.Reset()
+	g.Options.GeoM.Translate(float64(g.Player.X), float64(g.Player.Y))
 	// sub-rect is the width of a frame times the frame number, plus the frame number for the 1-pixel buffer between frames
-	screen.DrawImage(img.Image.SubImage(image.Rect(img.FrameWidth*imgFrameNum+imgFrameNum, 0, img.FrameWidth*imgFrameNum+imgFrameNum+img.FrameWidth, img.FrameHeight)).(*ebiten.Image), op)
+	screen.DrawImage(g.Player.Sprite.Image.SubImage(image.Rect(g.Player.Sprite.FrameWidth*g.Player.FrameNum+g.Player.FrameNum, 0, g.Player.Sprite.FrameWidth*g.Player.FrameNum+g.Player.FrameNum+g.Player.Sprite.FrameWidth, g.Player.Sprite.FrameHeight)).(*ebiten.Image), g.Options)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -269,7 +182,64 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 func main() {
 	ebiten.SetWindowSize(640, 480)
 	ebiten.SetWindowTitle("grame")
-	game := &Game{}
+
+	sprites, err := os.ReadFile("./sprites/sprites.json")
+	var jsonSprites []SpriteJSON
+	err = json.Unmarshal(sprites, &jsonSprites)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	linkSprites := map[string]Sprite{}
+	for _, v := range jsonSprites {
+		sprite, _, err := ebitenutil.NewImageFromFile("./sprites/"+v.Image, 0)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// camelCase the filenames without the extension to make sprite keys
+		var snek bool
+		var k string
+		image := v.Image[:len(v.Image)-4]
+		for i := 0; i < len(image); i++ {
+			char := string(image[i])
+			if char == "_" {
+				snek = true
+				continue
+			}
+			if snek == true {
+				snek = false
+				k += strings.ToUpper(char)
+			} else {
+				k += char
+			}
+		}
+
+		linkSprites[k] = Sprite{
+			FrameLen:    v.FrameLen,
+			FrameHeight: v.FrameHeight,
+			FrameWidth:  v.FrameWidth,
+			Image:       sprite,
+		}
+	}
+
+	sprite := linkSprites["linkStandSouth"]
+	op := &ebiten.DrawImageOptions{}
+
+	game := &Game{
+		Player: Player{
+			X:         0,
+			Y:         0,
+			KeyBuf:    -1,
+			LastDir:   ebiten.KeyDown,
+			Animation: false,
+			FrameNum:  0,
+			Sprite:    sprite,
+		},
+		Sprites: linkSprites,
+		Options: op,
+	}
+
 	if err := ebiten.RunGame(game); err != nil {
 		panic(err)
 	}
